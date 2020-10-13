ee027eeed92da33d4be559905f4e9920fcca3430
WeeMick <michaelblr5@gmail.com>
2020-10-14 00:16:11 +0100

    initial commit - Hello name working in url

---
+++ README.md
@@ -0,0 +1,4 @@
+symblog
+=======
+
+A Symfony project created on October 13, 2020, 10:44 pm.
---
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('@BloggerBlog/Default/index.html.twig',
+            array('name' => $name));
+
+    }
+}
