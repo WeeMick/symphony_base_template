b90911fab17081ca60c0e5c58d8be892d180c97b
WeeMick <michaelblr5@gmail.com>
2020-10-14 01:43:24 +0100

    sidebar showing, before making new controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Blogger\BlogBundle\BloggerBlogBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
